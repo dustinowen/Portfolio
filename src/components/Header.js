063fd5bd55d3723e23020884a55cbef54a03f701
dustinowen <137654369+dustinowen@users.noreply.github.com>
2023-09-01 11:37:01 -0700

    Update header - left two options to choose from, current active option is my current pick.

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,20 +3,20 @@ import "./header.css";
 import { Link } from "react-router-dom";
 
 export default function Header(props) {
-  const navStyle = {
-    display: "flex",
-    justifyContent: "space-around",
-    borderBottom: ".2rem solid black",
-    padding: ".5rem",
-    width: "90%",
-    margin: "auto",
-  };
+  // const navStyle = {
+  //   display: "flex",
+  //   justifyContent: "space-around",
+  //   borderBottom: ".2rem solid black",
+  //   padding: ".5rem",
+  //   width: "90%",
+  //   margin: "auto",
+  // };
 
   return (
     <header>
       <div className="wrapper">
-          <h1>Hello, my name is Dustin —
-          <Typed
+          <h1>Hello, my name is Dustin —<br/>
+          {/* <Typed
             strings={[
               "I am a JavaScript progr",
               "I am an Express.js novi",
@@ -25,8 +25,20 @@ export default function Header(props) {
         ]}
         typeSpeed={75}
         backSpeed={50}
-        backDelay={200}
-      /></h1>
+        backDelay={100}
+          /> */}
+          <Typed
+            strings={[
+              "I am a JavaScript programmer...",
+              "I am an Express.js novice...",
+              "I am a student of React...",
+              "I am a Full-Stack Developer!",
+        ]}
+        typeSpeed={75}
+        backSpeed={50}
+        backDelay={100}
+          />
+        </h1>
       </div>
     </header>
   );
